4da1da89d385e1c14168f032a16c740313de5743
= <=>
2022-09-04 12:22:43 +0200

    Added Question Types, Stepper and formatting
    Co-authored-by: dguttmann18 <dguttmann18@users.noreply.github.com>
    Co-authored-by: Carl Brann <BRNCAR035@myuct.ac.za>

---
+++ src/Components/BooleanQuestion.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Slider from '@mui/material/Slider';
+
+export class BooleanQuestion extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    
+    return (
+        <div>
+            <p>{this.props.question}</p>
+            <input type="radio" value="Yes" name="boolean"/> Yes
+            <input type="radio" value="No" name="boolean"/> No
+        </div>
+    )
+  }
+}
