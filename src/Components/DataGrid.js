cd4dbf7972d10cbddc273f3df336e3a7777181aa
= <=>
2022-09-05 11:50:58 +0200

    Added Datagrid working
    Co-authored-by: Carl Brann <BRNCAR035@myuct.ac.za>

--- src/Components/DataGrid.js
+++ src/Components/DataGrid.js
@@ -1,43 +1,59 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';  
+import ReactTable from "react-table-6";  
+import "react-table-6/react-table.css";  
+  
+class DataGrid extends Component {  
+  render() {  
+     const data = [{  
+        name: 'Ayaan', surname: "Kanji",
+        time: '15:26',  link: "View"  
+        },{  
+         name: 'Ahana', surname: "De Sousa", 
+         time: '12:56',  link: "View" 
+         },{  
+         name: 'Peter', surname: "Bartholomew",
+         time: '12:31' ,  link: "View"      
+         },{  
+         name: 'Virat', surname: "Cristwood",
+         time: "07:15" ,  link: "View" 
+         },{  
+         name: 'Rohit',  surname: "Swingler",
+         time: "09:00" ,  link: "View" 
+         },{  
+         name: 'Dhoni',  surname: "Olatundji",
+         time: "11:05"  ,  link: "View" 
+         }]  
 
-class DataGrid extends Component {
-
-   constructor(props) {
-
-      super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
-
-      this.state = { //state is by default an object
-
-         students: [
-
-            { id: 1, name: 'deepak', age: 21, email: 'deepak@email.com' },
-
-            { id: 2, name: 'arsad', age: 19, email: 'arsad@email.com' },
-
-            { id: 3, name: 'raman', age: 16, email: 'raman@email.com' },
-
-            { id: 4, name: 'ajeet', age: 25, email: 'ajeet@email.com' }
-
-         ]
-
+     const columns = [
+       {  
+       Header: 'Name',  
+       accessor: 'name'  
+       },
+       {  
+       Header: 'Surname',  
+       accessor: 'surname'  
+       },
+       {  
+      Header: 'Time',  
+      accessor: 'time'  
+      },
+      {  
+      Header: 'Link',  
+      accessor: 'link'  
       }
-
-   }
-
-   render() { //Whenever our class runs, the render method will be called automatically. It may have already been defined in the constructor behind the scene.
-
-      return (
-
-         <div>
-
-            
-
-         </div>
-
-      )
-
-   }
-
-}
-
-export default DataGrid 
+         
+      
+      ]  
+    return (  
+          <div>  
+              <ReactTable  
+                  data={data}  
+                  columns={columns}  
+                  defaultPageSize = {6}  
+                  pageSizeOptions = {[2,4, 6]}  
+              />  
+          </div>        
+    )  
+  }  
+}  
+export default DataGrid;  
